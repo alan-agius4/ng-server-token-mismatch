069d2b87fb32398b450e2321c2de52db72819b7b
Alan Agius <alanagius@google.com>
2022-08-11 09:08:10 +0000

    add usage of app_base_href

--- src/app/app-shell/app-shell.component.ts
+++ src/app/app-shell/app-shell.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-app-shell',
   templateUrl: './app-shell.component.html',
-  styleUrls: ['./app-shell.component.css']
+  styleUrls: ['./app-shell.component.css'],
 })
 export class AppShellComponent implements OnInit {
-
-  constructor() { }
+  // `APP_BASE_HREF` refers to the token in main-server.js
+  constructor(@Inject(APP_BASE_HREF) private appBaseHref: string | undefined) {}
 
   ngOnInit(): void {
+    console.log({ APP_BASE_HREF: this.appBaseHref });
   }
-
 }
